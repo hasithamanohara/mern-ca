25342b96e732df71683e3fe25955eda6dfead6da
hasithauom <manoharauh.20@uom.lk>
2023-11-28 09:55:31 +0530

    ft_home_updated

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -8,7 +8,7 @@ export default function MainPage() {
         adipisicing elit. Consequuntur deserunt nobis 
         temporibus soluta labore eaque illo. At 
         reiciendis quas fuga iste, sit unde et quaerat, 
-        facere omnis ex consequatur voluptatibus.</p>
+        facere omnis ex consequatur voluptatibus. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro quo iste veniam quia deleniti perferendis nostrum nesciunt rerum! Inventore libero velit placeat quam cupiditate excepturi molestiae maiores sapiente voluptates ea!</p>
     </div>
   )
 }
